efe11b8f6860af692b2d322e5dce30cfa24d30e2
BrahR <44587812+BrahR@users.noreply.github.com>
2023-11-17 10:31:32 -0800

    Removed unused CSS

--- FrontEndApp/src/components/layout/DashboardLayout.vue
+++ FrontEndApp/src/components/layout/DashboardLayout.vue
@@ -40,7 +40,7 @@ const closeCreateWorkspace = () => {
           </div>
 
           <div class="home-sidebar-title-icon-search">
-            <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                 <rect id="Rectangle" x="0" y="0" width="24" height="24"></rect>
                 <path
