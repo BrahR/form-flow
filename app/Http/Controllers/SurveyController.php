280a4d607f269567239eb6fa0d8411d2fd479ab4
BrahR <44587812+BrahR@users.noreply.github.com>
2023-11-17 10:26:34 -0800

    Index and show methods

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -10,6 +10,21 @@ use DeepCopy\Filter\ReplaceFilter;
 use Illuminate\Http\Response;
 
 class SurveyController extends Controller {
+
+    public function index(Workspace $workspace): Response {
+        $surveys = $workspace->surveys()->get();
+
+        return response([
+            "surveys" => $surveys,
+        ]);
+    }
+
+    public function show(Survey $survey): Response {
+        return response([
+            "survey" => $survey,
+        ]);
+    }
+
     public function store(SurveyRequest $request, Workspace $workspace): Response {
 //        $survey = Survey::create($request->validated());
         $survey = $workspace->surveys()->create($request->validated());
@@ -20,10 +35,10 @@ class SurveyController extends Controller {
     }
 
     public function update(SurveyRequest $request, Survey $survey): Response {
-
+        $survey->update($request->validated());
 
         return response([
-
+            "survey" => $survey,
         ]);
     }
 }
