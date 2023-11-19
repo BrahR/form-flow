44146f65469362a669c11656000c88653e951fda
BrahR <44587812+BrahR@users.noreply.github.com>
2023-11-19 12:42:28 -0800

    Coming soon

--- FrontEndApp/src/router/index.ts
+++ FrontEndApp/src/router/index.ts
@@ -6,108 +6,114 @@ import Register from "@/views/Register.vue";
 import Dashboard from "@/views/Dashboard.vue";
 import SurveyBuild from "@/views/survey/SurveyBuild.vue";
 import HelloWorld from "@/components/HelloWorld.vue";
-import SurveyDesign from "@/views/survey/SurveyDesign.vue";
-import SurveyResults from "@/views/survey/SurveyResults.vue";
-import SurveySettings from "@/views/survey/SurveySettings.vue";
-import SurveyShare from "@/views/survey/SurveyShare.vue";
+// import SurveyDesign from "@/views/survey/SurveyDesign.vue";
+// import SurveyResults from "@/views/survey/SurveyResults.vue";
+// import SurveySettings from "@/views/survey/SurveySettings.vue";
+// import SurveyShare from "@/views/survey/SurveyShare.vue";
 import SurveyLayout from "@/components/layout/SurveyLayout.vue";
 
 const routes: RouteRecordRaw[] = [
-    {
-        path: "/auth",
-        redirect: "/login",
-        name: "Auth",
-        meta: {requiresAuth: false},
-        children: [
-            {
-                path: "/login",
-                name: "Login",
-                component: Login
-            },
-            {
-                path: "/register",
-                name: "Register",
-                component: Register
-            },
-        ]
-    },
-    {
-        path: "/",
-        redirect: "/dashboard",
+  {
+    path: "/auth",
+    redirect: "/login",
+    name: "Auth",
+    meta: {requiresAuth: false},
+    children: [
+      {
+        path: "/login",
+        name: "Login",
+        component: Login
+      },
+      {
+        path: "/register",
+        name: "Register",
+        component: Register
+      },
+    ]
+  },
+  {
+    path: "/",
+    redirect: "/dashboard",
+    name: "Dashboard",
+    meta: {requiresAuth: true},
+    children: [
+      {
+        path: "/dashboard",
         name: "Dashboard",
-        meta: {requiresAuth: true},
-        children: [
-            {
-                path: "/dashboard",
-                name: "Dashboard",
-                component: Dashboard
-            }
-        ]
-    },
-    {
-        path: "/w-:workspaceId/survey/:surveyId",
-        meta: {requiresAuth: true},
-        component: SurveyLayout,
-        children: [
-            {
-                path: "",
-                alias: "build",
-                name: "Survey.Build",
-                component: SurveyBuild
-            },
-            {
-                path: "design",
-                name: "Survey.Design",
-                component: SurveyDesign
-            },
-            {
-                path: "results",
-                name: "Survey.Results",
-                component: SurveyResults
-            },
-            {
-                path: "settings",
-                name: "Survey.Settings",
-                component: SurveySettings
-            },
-            {
-                path: "share",
-                name: "Survey.Share",
-                component: SurveyShare
-            },
-            {
-                path: "preview",
-                name: "Survey.Preview",
-                component: HelloWorld
-            },
-            {
-                path: "responses",
-                name: "Survey.Responses",
-                component: HelloWorld
-            },
-        ]
-    },
-    {
-        path: "/test",
-        name: "Test",
-        component: HelloWorld,
-    },
+        component: Dashboard
+      }
+    ]
+  },
+  {
+    path: "/w-:workspaceId/survey/:surveyId",
+    meta: {requiresAuth: true},
+    component: SurveyLayout,
+    children: [
+      {
+        path: "",
+        alias: "build",
+        name: "Survey.Build",
+        component: SurveyBuild
+      },
+      {
+        path: "design",
+        name: "Survey.Design",
+        // component: SurveyDesign
+        component: HelloWorld
+      },
+      {
+        path: "results",
+        name: "Survey.Results",
+        // component: SurveyResults
+        component: HelloWorld
+      },
+      {
+        path: "settings",
+        name: "Survey.Settings",
+        // component: SurveySettings
+        component: HelloWorld
+      },
+      {
+        path: "share",
+        name: "Survey.Share",
+        // component: SurveyShare
+        component: HelloWorld
+      },
+      {
+        path: "preview",
+        name: "Survey.Preview",
+        // component: HelloWorld
+        component: HelloWorld
+      },
+      {
+        path: "responses",
+        name: "Survey.Responses",
+        // component: HelloWorld
+        component: HelloWorld
+      },
+    ]
+  },
+  {
+    path: "/test",
+    name: "Test",
+    component: HelloWorld,
+  },
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes
 });
 
 router.beforeEach(async (to, from, next) => {
-    from.meta.isAuth = from.meta.isAuth ?? false
-    const useUser = useUserStore()
-    if (useUser.getToken()) useUser.hydrate().catch(() => console.log("E"))
+  from.meta.isAuth = from.meta.isAuth ?? false
+  const useUser = useUserStore()
+  if (useUser.getToken()) useUser.hydrate().catch(() => console.log("E"))
 
-    if (to.meta.requiresAuth && !useUser.getToken()) return next({name: "Login"})
-    if (!to.meta.requiresAuth && useUser.getToken()) return next({name: "Dashboard"})
+  if (to.meta.requiresAuth && !useUser.getToken()) return next({name: "Login"})
+  if (!to.meta.requiresAuth && useUser.getToken()) return next({name: "Dashboard"})
 
-    return next();
+  return next();
 })
 
 export default router as Router;
