589cebaae8286aaf675573cfa218b87d210eb796
Abd <44587812+BrahR@users.noreply.github.com>
2024-02-23 05:45:15 -0500

    Fix: corresponding changes to the migration changes

--- vue-app/src/types/modals/Question.ts
+++ vue-app/src/types/modals/Question.ts
@@ -1,7 +1,8 @@
 type Question<T extends Questionable = Questionable> = {
   id: number;
   html_label: string;
-  description_label: string;
+  described: boolean;
+  html_description: string | null;
   attachment: string | null;
   required: boolean;
   questionable: T;
